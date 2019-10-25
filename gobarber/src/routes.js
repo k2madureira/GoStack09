5adb082d55791504a88c172bc123c96ca45ebf1d
k2madureira <lenilsonmadureira@gmail.com>
2019-10-24 22:22:41 -0300

    gostack

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,9 +1,31 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.get('/', (req, res)=>{
-  return res.json({msg:'hello'});
-});
+routes.post('/user', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/user', UserController.update);
+routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
